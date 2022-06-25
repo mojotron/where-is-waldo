abbeb9a5608ddc8ebe2673066941f48dcb47e734
mojotron <igormalj@gmail.com>
2022-06-25 22:32:49 +0200

    update style

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { uploadImageToStorage } from "../firebaseApp";
+import "../styles/Dashboard.css";
+import uploadImgIcon from "../images/image-svgrepo-com.svg";
 
 const Dashboard = () => {
   const [uploadImage, setUploadImage] = useState(null);
@@ -15,14 +17,22 @@ const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <input
-        type="file"
-        accept="image/png image/jpg"
-        onChange={(e) => setUploadImage(e.target.files[0])}
-      />
-      <button type="button" onClick={handleUploadImage}>
-        upload image
-      </button>
+      <div className="">
+        <h2>Add new image</h2>
+        <label className="input-type-file" htmlFor="upload-image">
+          {uploadImage ? uploadImage : "Select Image"}
+          <img src={uploadImgIcon} className="icon" />
+          <input
+            id="upload-image"
+            type="file"
+            accept="image/png image/jpg"
+            onChange={(e) => setUploadImage(e.target.files[0])}
+          />
+        </label>
+        <button className="btn" type="button" onClick={handleUploadImage}>
+          upload image
+        </button>
+      </div>
     </div>
   );
 };
